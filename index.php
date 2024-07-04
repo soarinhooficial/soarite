bbb397186659ea892268953988aa3b02fd4c73e5
lucasdosite <lucassoares112233@gmail.com>
2024-07-04 13:42:14 -0300

    Revoluciotech

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php include './components/head.php'; ?>
+</head>
+
+<body>
+
+    <header>
+        <?php include './components/header.php'; ?>
+    </header>
+
+    <main class="d-flex flex-column min-vh-100">
+        <?php include './pages/home.php'; ?>
+    </main>
+
+    <footer>
+        <?php include './components/footer.php'; ?>
+    </footer>
+
+</body>
+
+</html>
---
+++ pages/home.php
@@ -0,0 +1,8 @@
+<section id="section">
+    <div class="container pt-5">
+        <h1>artes e tecnologia de diversas perspectivas.</h1>
+        <h4>Soluções em Audiovisual e Informática</h4>
+        <br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aut eos atque molestiae praesentium ad, eveniet ut nostrum quis minus, laudantium porro corrupti quas error at fugiat magnam nulla in!</p>
+    </div>
+</section>
