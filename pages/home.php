19b86f017aca213b65b242bd3b8b1522b2854b00
lucasdosite <lucassoares112233@gmail.com>
2024-07-04 13:50:17 -0300

    Update

--- pages/home.php
+++ pages/home.php
@@ -3,6 +3,20 @@
         <h1>artes e tecnologia de diversas perspectivas.</h1>
         <h4>Soluções em Audiovisual e Informática</h4>
         <br>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aut eos atque molestiae praesentium ad, eveniet ut nostrum quis minus, laudantium porro corrupti quas error at fugiat magnam nulla in!</p>
+        <p>Perfeccionista, Determinado e Proativo, Lucas Soares (Soarinho) é um Analista de Sistemas Desenvolvedor Front-End, apaixonado por criar Websites que transformam ideias em realidade, seja para negócios ou uso pessoal.</p>
+
+        <p>Seu foco principal está na Stack Front-End para Desenvolvimento de Sistemas, Websites e Apps. Valoriza a ordem, respeito e organização em seu trajeto profissional, sempre prezando pelo bom atendimento, responsabilidade e compromisso com o serviço.</p>
+
+        <p>Além de desenvolver, se aventura no mundo do Audiovisual, oferecendo serviços de Música ao Vivo no Violoncelo para Eventos, Locução e Dublagem, Edição de Áudio, Vídeo e Imagens e Design Digital.</p>
+
+        <p>Com experiência prévia em Suporte Técnico de Informática, incluindo Montagem e Manutenção de Computadores, traz uma bagagem adicional que complementa minha trajetória hoje.</p>
+
+        <p>E como hobbies, é apaixonado por Moda Masculina, e é Artista independente nos ramos musical, visual e literário, encontrando inspiração em cada projeto.</p>
+
+        <p>Foi Estagiário de Tecnologia da Informação no serviço público, começando pelo setor de Manutenção e Suporte Técnico. Também prestou breve auxílio ao setor Administrativo do mesmo departamento, e atuou no setor de Desenvolvimento de Sistemas, realizando tarefas relacionadas ao Front-End de Sistemas Web e Websites. Setor este em que se encontra hoje como Coordenador Técnico de Desenvolvimento e Programação;</p>
+
+        <p>Já foi Monitor de Alunos (com ênfase em cursos de informática e tecnologia da informação) em uma instituição de ensino profissionalizante, e também Instrutor de aulas práticas de Montagem e Manutenção de Computadores, na mesma instituição;</p>
+
+        <p>Foi Assistente Comercial para uma Agência de Turismo de Angra dos Reis;</p>
     </div>
 </section>
