df0f0c0140c9c843aacd360e42e061f08bc6367f
lucasdosite <lucassoares112233@gmail.com>
2024-07-24 11:33:43 -0300

    teste

--- index.php
+++ index.php
@@ -12,49 +12,44 @@ include_once 'globals.php';
 <body id="page-top">
 
     <header>
-        <?php include __DIR__ . DS .'/elements/header.php'; ?>
+        <?php include __DIR__ . DS . '/elements/header.php'; ?>
     </header>
     <main id="contentPage" class="d-flex flex-column min-vh-100">
 
         <?php
-        // Captura a URI amigável da URL
         $uri = isset($_SERVER['REQUEST_URI']) ? trim($_SERVER['REQUEST_URI'], '/') : 'inicio';
 
-        // Remove qualquer parâmetro de query string
         $uri = strtok($uri, '?');
 
-        // Remove o prefixo do caminho (se houver)
         $uri = str_replace('governodigital.angra.rj.gov.br/', '', $uri);
         $uri = ltrim($uri, '/');
 
-        // Define a página padrão como 'inicio' se a URI estiver vazia
         if ($uri == '') {
             $uri = 'inicio';
         }
 
-        // Mapeamento das páginas
         $pages = [
-            'inicio' => __DIR__ . DS .'/pages/home.php',
-            'egd' => __DIR__ . DS .'/pages/egd.php',
-            'ceti' => __DIR__ . DS .'/pages/ceti.php',
-            'servicos-digitais' => __DIR__ . DS .'/pages/servicos-digitais.php',
-            'processo-eletronico' => __DIR__ . DS .'/pages/processo-eletronico.php',
-            'forum-de-transformacao-digital' => __DIR__ . DS .'/pages/forum-de-transformacao-digital.php',
-            'boletim' => __DIR__ . DS .'/pages/boletim.php',
-            'contato' => __DIR__ . DS .'/pages/contato.php',
+            'inicio' => __DIR__ . DS . '/pages/home.php',
+            'egd' => __DIR__ . DS . '/pages/egd.php',
+            'ceti' => __DIR__ . DS . '/pages/ceti.php',
+            'servicos-digitais' => __DIR__ . DS . '/pages/servicos-digitais.php',
+            'processo-eletronico' => __DIR__ . DS . '/pages/processo-eletronico.php',
+            'forum-de-transformacao-digital' => __DIR__ . DS . '/pages/forum-de-transformacao-digital.php',
+            'boletim' => __DIR__ . DS . '/pages/boletim.php',
+            'contato' => __DIR__ . DS . '/pages/contato.php',
         ];
 
         // Incluir a página correta ou a página de erro 404
         if (array_key_exists($uri, $pages)) {
             include $pages[$uri];
         } else {
-            include __DIR__ . DS .'/pages/erro404.php';
+            include __DIR__ . DS . '/pages/erro404.php';
         }
         ?>
 
     </main>
     <footer>
-        <?php include __DIR__ . DS .'/elements/footer.php'; ?>
+        <?php include __DIR__ . DS . '/elements/footer.php'; ?>
     </footer>
 </body>
 
